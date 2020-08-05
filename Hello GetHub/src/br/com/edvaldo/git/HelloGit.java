f9cab9ca2fc88b8d14b3e41f97f583f96c895a5b
edvaldoproo <68858554+edvaldoproo@users.noreply.github.com>
2020-08-05 09:32:23 -0300

    Adicionei arquivo via upload

---
+++ Hello GetHub/src/br/com/edvaldo/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.edvaldo.git;
+
+/**
+ * @author Edvaldo
+ * Exemplo de integração com o GitHub
+ */
+public class HelloGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+System.out.println("Hello Git");
+
+	}
+
+}
